079d569e1ab84e8845cc9b0f316efc2ad43ce9e6
Ayyappa <ayyappa.99@gmail.com>
2025-08-20 19:15:01 +0530

    chore: updated docs

--- api/docs/v3/d2/d13/class_voxel_busters_1_1_essential_kit_1_1_rate_my_app.js
+++ api/docs/v3/d2/d13/class_voxel_busters_1_1_essential_kit_1_1_rate_my_app.js
@@ -1,6 +1,6 @@
 var class_voxel_busters_1_1_essential_kit_1_1_rate_my_app =
 [
-    [ "Initialize", "d2/d13/class_voxel_busters_1_1_essential_kit_1_1_rate_my_app.html#a8aa5370e8d89923497bca6c9ed768638", null ],
+    [ "Initialize", "d2/d13/class_voxel_busters_1_1_essential_kit_1_1_rate_my_app.html#ae7f190f6860be761a5075ef9602e5a35", null ],
     [ "IsAllowedToRate", "d2/d13/class_voxel_busters_1_1_essential_kit_1_1_rate_my_app.html#aa51606bd59d1b9c673303112d2d322b4", null ],
     [ "AskForReviewNow", "d2/d13/class_voxel_busters_1_1_essential_kit_1_1_rate_my_app.html#a376750c717b11f2687300067f0c2192e", null ],
     [ "OnConfirmationPromptAction", "d2/d13/class_voxel_busters_1_1_essential_kit_1_1_rate_my_app.html#ae596911aecad9c4db182ed37e3530649", null ]
--- api/docs/v3/d2/da4/class_voxel_busters_1_1_essential_kit_1_1_notification_android_properties.js
+++ api/docs/v3/d2/da4/class_voxel_busters_1_1_essential_kit_1_1_notification_android_properties.js
@@ -2,5 +2,6 @@ var class_voxel_busters_1_1_essential_kit_1_1_notification_android_properties =
 [
     [ "Tag", "d2/da4/class_voxel_busters_1_1_essential_kit_1_1_notification_android_properties.html#a121d9cb485fa9e23380cb8c883e46c9c", null ],
     [ "LargeIcon", "d2/da4/class_voxel_busters_1_1_essential_kit_1_1_notification_android_properties.html#adcbc8416bde8c3807df00a5c696b12c1", null ],
-    [ "BigPicture", "d2/da4/class_voxel_busters_1_1_essential_kit_1_1_notification_android_properties.html#af48527e1b6e750e7a019b908b3acbcc3", null ]
+    [ "BigPicture", "d2/da4/class_voxel_busters_1_1_essential_kit_1_1_notification_android_properties.html#af48527e1b6e750e7a019b908b3acbcc3", null ],
+    [ "Channel", "d2/da4/class_voxel_busters_1_1_essential_kit_1_1_notification_android_properties.html#a463b4f79512c1d441ad0876f82d1ca96", null ]
 ];
